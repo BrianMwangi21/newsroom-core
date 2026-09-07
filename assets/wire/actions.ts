fef17abc202c96ef725c3b8d40ebaa62138ddfc5
Andrew Marwood <amarwood@aap.com.au>
2026-09-07 17:28:01 +1000

    SDAN-773 Save download files with correct name and extension (#1559)
    * SDAN-773 Save download files with correct name and extension
    
    * Updated after review

--- assets/wire/actions.ts
+++ assets/wire/actions.ts
@@ -465,8 +465,18 @@ export function submitDownloadItems(items: any, params: any) {
         else{
             try {
                 const response = await server.post(url, payload, undefined, {parseJson: false});
+
+                let filename = '';
+                const contentDisposition = response.headers.get('content-disposition');
+                if (contentDisposition) {
+                    const match = contentDisposition.match(/filename="?([^";]+)"?/);
+                    if (match && match.length > 1) {
+                        filename = match[1];
+                    }
+                }
+
                 const blob = await response.blob();
-                initiateDownload(blob);
+                initiateDownload(blob, filename);
             } catch (error: any) {
                 errorHandler(error);
             }
@@ -477,11 +487,25 @@ export function submitDownloadItems(items: any, params: any) {
     };
 }
 
-function initiateDownload(source: string | Blob) {
+function initiateDownload(source: string | Blob, filename = '') {
     const link = document.createElement('a');
-    link.download = '';
-    link.href = typeof source === 'string' ? source : URL.createObjectURL(source);
+
+    if (filename) {
+        link.setAttribute('download', filename);
+    } else {
+        link.setAttribute('download', '');
+    }
+
+    const url = typeof source === 'string' ? source : URL.createObjectURL(source);
+    link.href = url;
+
+    document.body.appendChild(link);
     link.click();
+    document.body.removeChild(link);
+
+    if (typeof source !== 'string') {
+        setTimeout(() => URL.revokeObjectURL(url), 100);
+    }
 }
 
 
